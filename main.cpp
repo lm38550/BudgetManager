a6943d72d211327af4b9a8f4f71332d7a35f36eb
Morel Louis <louis.morel@etu.univ-lyon1.fr>
2025-05-03 20:35:52 +0200

    Updating test database + Added test button in menu

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
  
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
+#include "dataAcces.h"
  
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
@@ -15,10 +16,13 @@ public:
  
 class MyFrame : public wxFrame
 {
+private:
+    dataAcces data;
 public:
     MyFrame();
  
 private:
+    void OnTest(wxCommandEvent& event);
     void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnOther(wxCommandEvent& event);
@@ -28,7 +32,8 @@ private:
 enum
 {
     ID_Hello = 1,
-    ID_Other = 2
+    ID_Other = 2,
+    ID_Test = 3
 };
  
 wxIMPLEMENT_APP(MyApp);
@@ -46,6 +51,8 @@ MyFrame::MyFrame()
     wxMenu *menuFile = new wxMenu;
     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
                      "Help string shown in status bar for this menu item");
+    menuFile->Append(ID_Test, "&Test",
+                     "Testing app functions");
     menuFile->AppendSeparator();
     menuFile->Append(wxID_EXIT);
  
@@ -67,6 +74,7 @@ MyFrame::MyFrame()
     SetStatusText("Welcome to wxWidgets!");
  
     Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+    Bind(wxEVT_MENU, &MyFrame::OnTest, this, ID_Test);
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnOther, this, ID_Other);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
@@ -90,5 +98,17 @@ void MyFrame::OnOther(wxCommandEvent& event)
  
 void MyFrame::OnHello(wxCommandEvent& event)
 {
-    wxLogMessage("Hello world from wxWidgets!");
+    wxLogMessage("Hello world from wxWidgets!");    
+}
+
+void MyFrame::OnTest(wxCommandEvent& event)
+{
+    wxLogMessage("Look at console, functions are being tested !");
+    vector<int> IDcat;
+    vector<string> NameCat;
+    data.getCategoryList(IDcat, NameCat);
+    data.getAccountBalanceByID(1);
+    data.getAccountIDByName("Test2");
+    data.getAccountNameByID(1);
+    
 }
